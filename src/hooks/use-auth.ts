911f5af6749a9c99dd1ee036e2710a8ca0de57a0
Denis Lednev <denzik252@gmail.com>
2024-04-04 23:13:28 +0300

    Build changes

--- src/hooks/use-auth.ts
+++ src/hooks/use-auth.ts
@@ -5,28 +5,28 @@ export const useAuth = () => {
   const router = useRouter()
 
   const signOut = async () => {
-    try {
-      const res = await fetch(
-        `${process.env.NEXT_PUBLIC_SERVER_URL}/api/users/logout`,
-        {
-          method: 'POST',
-          credentials: 'include',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          cache: 'no-store',
-        }
-      )
+    // try {
+    //   const res = await fetch(
+    //     `${process.env.NEXT_PUBLIC_SERVER_URL}/api/users/logout`,
+    //     {
+    //       method: 'POST',
+    //       credentials: 'include',
+    //       headers: {
+    //         'Content-Type': 'application/json',
+    //       },
+    //       cache: 'no-store',
+    //     }
+    //   )
 
-      if (!res.ok) throw new Error()
+    //   if (!res.ok) throw new Error()
 
-      toast.success('Signed out successfully')
+    //   toast.success('Signed out successfully')
 
-      router.push('/sign-in')
-      router.refresh()
-    } catch (err) {
-      toast.error("Couldn't sign out, please try again.")
-    }
+    //   router.push('/sign-in')
+    //   router.refresh()
+    // } catch (err) {
+    //   toast.error("Couldn't sign out, please try again.")
+    // }
   }
 
   return { signOut }
